e120a49783cf0b5b080b146a7c854c171a04ae5a
dimaslopez2003 <21393126@utcancun.edu.mx>
2024-12-02 16:13:13 -0500

    BLogAPp

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.google.firebase.crashlytics") version "2.9.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
     alias(libs.plugins.google.gms.google.services) apply false
 }
